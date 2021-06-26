cf5884f5460ec05d1399ee381d9b082b7314ec78
OOCLDM\HUANGHA4 <huangha4@oocl.com>
2021-06-26 15:35:03 +0800

    [Poker, Refactor] Update poker compare result method logic

--- src/main/java/com/thoughtworks/refactor/Hands.java
+++ src/main/java/com/thoughtworks/refactor/Hands.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.refactor;
 
+import java.util.*;
+
 public class Hands {
     private final String hands;
 
@@ -17,5 +19,41 @@ public class Hands {
 
     public Catetgory getCategory() {
         return new Catetgory(PokerUtil.judgeHandCategory(this.hands));
+    }
+
+    public int[] getDistinctDescendingHandsNumbers() {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] number = getDescendingHandsNumbers();
+        for (int i = 0; i < number.length; i++) {
+            if (map.get(number[i]) != null) {
+                map.put(number[i], map.get(number[i]) + 1);
+            } else {
+                map.put(number[i], 1);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
+        list.addAll(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
+                return arg1.getValue().compareTo(arg0.getValue());
+            }
+        });
+        int[] arrayresult = new int[list.size()];
+        int i = 0;
+        for (Map.Entry<Integer, Integer> entry : list) {
+            arrayresult[i] = entry.getKey();
+            i++;
+        }
+        return arrayresult;
+    }
+
+    int[] getDescendingNoRepeatNumbers() {
+        int[] descendingHandsNumbers = getDescendingHandsNumbers();
+        return PokerUtil.noOrRepeatNumber(descendingHandsNumbers, 1);
+    }
+
+    int[] getDescendingRepeatNumbers() {
+        int[] descendingHandsNumbers = getDescendingHandsNumbers();
+        return PokerUtil.noOrRepeatNumber(descendingHandsNumbers, 0);
     }
 }
--- src/main/java/com/thoughtworks/refactor/Poker.java
+++ src/main/java/com/thoughtworks/refactor/Poker.java
@@ -14,18 +14,18 @@ public class Poker {
         final Catetgory blackHandsCategory = blackHandsObj.getCategory();
         int blackHandsCategoryRanking = blackHandsCategory.judgeHandsCategoryRanking();
 
-        int[] blackDistinctDescendingHandsNumbers = getDistinctDescendingHandsNumbers(blackDescendingHandsNumbers);
-        int[] blackRepeatNumbers = getDescendingRepeatNumbers(blackDescendingHandsNumbers);
-        int[] blackNoRepeatNumbers = getDescendingNoRepeatNumbers(blackDescendingHandsNumbers);
+        int[] blackDistinctDescendingHandsNumbers = blackHandsObj.getDistinctDescendingHandsNumbers();
+        int[] blackRepeatNumbers = blackHandsObj.getDescendingRepeatNumbers();
+        int[] blackNoRepeatNumbers = blackHandsObj.getDescendingNoRepeatNumbers();
 
         final Hands whiteHandsObj = new Hands(whiteHands);
         int[] whiteDescendingHandsNumbers = whiteHandsObj.getDescendingHandsNumbers();
         final Catetgory whiteHandsCategory = whiteHandsObj.getCategory();
         int whiteHandsCategoryRanking = whiteHandsCategory.judgeHandsCategoryRanking();
 
-        int[] whiteDistinctDescendingHandsNumbers = getDistinctDescendingHandsNumbers(whiteDescendingHandsNumbers);
-        int[] whiteRepeatNumbers = getDescendingRepeatNumbers(whiteDescendingHandsNumbers);
-        int[] whiteNoRepeatNumbers = getDescendingNoRepeatNumbers(whiteDescendingHandsNumbers);
+        int[] whiteDistinctDescendingHandsNumbers = whiteHandsObj.getDistinctDescendingHandsNumbers();
+        int[] whiteRepeatNumbers = whiteHandsObj.getDescendingRepeatNumbers();
+        int[] whiteNoRepeatNumbers = whiteHandsObj.getDescendingNoRepeatNumbers();
 
         if (blackHandsCategoryRanking < whiteHandsCategoryRanking) {
             winResult = "black wins - " + CATEGORIES[blackHandsCategoryRanking];
@@ -154,114 +154,9 @@ public class Poker {
         return winResult;
     }
 
-    private int[] getDescendingNoRepeatNumbers(int[] blackDescendingHandsNumbers) {
-        return noOrRepeatNumber(blackDescendingHandsNumbers, 1);
-    }
-
-    private int[] getDescendingRepeatNumbers(int[] blackDescendingHandsNumbers) {
-        return noOrRepeatNumber(blackDescendingHandsNumbers, 0);
-    }
-
     private String intNumber(int i) {
         String[] strNumber = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
         return strNumber[i - 2];
-    }
-
-    private int[] getDistinctDescendingHandsNumbers(int[] number) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < number.length; i++) {
-            if (map.get(number[i]) != null) {
-                map.put(number[i], map.get(number[i]) + 1);
-            } else {
-                map.put(number[i], 1);
-            }
-        }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
-        list.addAll(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
-                return arg1.getValue().compareTo(arg0.getValue());
-            }
-        });
-        int[] arrayresult = new int[list.size()];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : list) {
-            arrayresult[i] = entry.getKey();
-            i++;
-        }
-        return arrayresult;
-    }
-
-    // First get the number of occurrences of each element in the array,
-    // then calculate the number of occurrences greater than 1 and the number of occurrences equal to 1.
-    private int[] noOrRepeatNumber(int[] number, int flag) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < number.length; i++) {
-            if (map.get(number[i]) != null) {
-                map.put(number[i], map.get(number[i]) + 1);
-            } else {
-                map.put(number[i], 1);
-            }
-        }
-        List<Map.Entry<Integer, Integer>> list = new ArrayList<Map.Entry<Integer, Integer>>();
-        list.addAll(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            public int compare(Map.Entry<Integer, Integer> arg0, Map.Entry<Integer, Integer> arg1) {
-                return arg1.getValue().compareTo(arg0.getValue());
-            }
-        });
-        int[] repeatnumber = new int[list.size()];
-        int[] norepeatnumber = new int[list.size()];
-        int i = 0;
-        if (flag == 0) {
-            for (Map.Entry<Integer, Integer> entry : list) {
-                if (entry.getValue() > 1) {
-                    repeatnumber[i] = entry.getKey();
-                    i++;
-                }
-            }
-        } else {
-            for (Map.Entry<Integer, Integer> entry : list) {
-                if (entry.getValue() == 1) {
-                    norepeatnumber[i] = entry.getKey();
-                    i++;
-                }
-            }
-        }
-        HashSet<Integer> hashSet = new HashSet<Integer>();
-        if (flag == 0) {
-            for (i = 0; i < repeatnumber.length; i++) {
-                hashSet.add(repeatnumber[i]);
-            }
-        } else {
-            for (i = 0; i < norepeatnumber.length; i++) {
-                hashSet.add(norepeatnumber[i]);
-            }
-        }
-        hashSet.remove(0);
-        int[] result = new int[hashSet.size()];
-        i = 0;
-        Iterator<Integer> iterator = hashSet.iterator();
-        while (iterator.hasNext()) {
-            result[i] = iterator.next();
-            i++;
-        }
-        int[] reResult = new int[result.length];
-        for (i = 0; i < result.length; i++) {
-            reResult[i] = result[result.length - i - 1];
-        }
-        return reResult;
-    }
-
-    private int judgeHandsCategoryRanking(String strType) {
-        int index = -1;
-        String[] type = {"StraightFlush", "FourOfAKind", "FullHouse", "Flush", "Straight", "ThreeOfAKind", "TwoPair", "OnePair", "HighCard"};
-        for (int i = 0; i < 9; i++) {
-            if (type[i].equals(strType)) {
-                index = i;
-            }
-        }
-        return index;
     }
 
 }
